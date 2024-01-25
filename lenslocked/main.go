2129df407632bd79310a731839184b0a9b5e86db
Kay Freyer <kay.freyer@icloud.com>
2024-01-25 14:24:25 +0100

    2.8

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,9 +5,31 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// pathHandler(w, r)
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", pathHandler)
-	http.ListenAndServe(":3000", nil)
+	// https://devdocs.io/go/net/http/index#HandleFunc
+	// HandleFunc registers the handler function for the given pattern in the
+	// DefaultServeMux. The documentation for ServeMux explains how patterns are
+	// matched.
+	// http.HandleFunc("/", pathHandler)
+
+	// ListenAndServe listens on the TCP network address addr and then calls Serve
+	// with handler to handle requests on incoming connections. Accepted connections
+	// are configured to enable TCP keep-alives.
+	http.ListenAndServe(":3000", &Router{})
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
