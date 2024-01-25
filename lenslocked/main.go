81607668aae874dfa40a39f1689f18e53e4deee2
Kay Freyer <kay.freyer@icloud.com>
2024-01-25 17:58:31 +0100

    3.3

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -7,13 +7,14 @@ import (
 
 type Router struct{}
 
-func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// pathHandler(w, r)
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
@@ -29,23 +30,20 @@ func main() {
 	// ListenAndServe listens on the TCP network address addr and then calls Serve
 	// with handler to handle requests on incoming connections. Accepted connections
 	// are configured to enable TCP keep-alives.
-	http.ListenAndServe(":3000", &Router{})
-}
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
+	var router Router
+	fmt.Println("Starting the server on: 3000...")
+	http.ListenAndServe(":3000", router)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calho")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Do you have questions?</p>")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
