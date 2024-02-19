860e776f3267dfcf75ec1fd5b2b64dc60411aa87
Kay Freyer <kay.freyer@icloud.com>
2024-02-19 06:58:36 +0100

    asdf

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	// "github.com/go-chi/chi/v5/middleware"
 )
 
 type Router struct{}
@@ -31,22 +38,57 @@ func main() {
 	// with handler to handle requests on incoming connections. Accepted connections
 	// are configured to enable TCP keep-alives.
 
-	var router Router
+	r := chi.NewRouter()
+	// r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Route("/contact", func(r chi.Router) {
+		r.Use(middleware.Logger)
+		r.Get("/", contactHandler)
+	})
+	r.Get("/faq", faqHandler)
+	r.Get("/gallery/{galleryID}", galleryHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not foundicilious", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on: 3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calho")
+	tplPath := filepath.Join("templates", "contact.html")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Do you have questions?</p>")
+	tplPath := filepath.Join("templates", "faq.html")
+	executeTemplate(w, tplPath)
+
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.html")
+	executeTemplate(w, tplPath)
+}
+
+func galleryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site</h1>")
+	fmt.Fprintf(w, "<h1>You requested gallery number %s</h1>", chi.URLParam(r, "galleryID"))
+	return
 }
